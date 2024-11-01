0c00c190c5f1cf84ee717b8b888077a51882c884
swatwas <swaythav16@gmail.com>
2024-11-02 01:24:06 +0800

    food tracker

--- Backend/foodtracker/usda.py
+++ Backend/foodtracker/usda.py
@@ -1,21 +1,28 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-def get_usda_nutrition(api_key, food_name):
+load_dotenv()
+
+def get_usda_nutrition(food_name):
     """Fetches nutrition data for a given food item from the USDA API."""
+    api_key = os.getenv("API_KEY")  
     base_url = "https://api.nal.usda.gov/fdc/v1/foods/search"
+    
+    # Parameters for the API request
     params = {
-        "api_key": "whu22Y5p90dlqijPy6Jmi2sw8dHh0oK8rfO4RnaY",
+        "api_key": api_key,
         "query": food_name,
-        "pageSize": 1  # Return only the top result
+        "pageSize": 1
     }
     
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
         data = response.json()
         
-        # Check if any food results were returned
+
         if "foods" in data and len(data["foods"]) > 0:
-            food_data = data["foods"][0]  # Take the first result
+            food_data = data["foods"][0]  
             nutrients = {n['nutrientName']: n['value'] for n in food_data['foodNutrients']}
             return {
                 "description": food_data.get("description", "Unknown"),
